a2b206bc219fca72634b12f9d714fd223252865a
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-09 16:31:57 -0400

    commit tree bf211aa2dbf5568f879ed992ba8e6b7f3af217a3

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(countdown_to_midnight)
+  while countdown_to_midnight.positive?
+    puts "#{countdown_to_midnight} SECOND(S)!"
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(countdown_to_midnight)
+  while countdown_to_midnight.positive?
+    puts "#{countdown_to_midnight} SECOND(S)!"
+    sleep(1)
+    countdown_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+#use "sleep()" for pauses between the countdown. Has to be written BEFORE 
+#the countdown
